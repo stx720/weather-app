576a815837b40dd7d85a287549cb86b0f938f35a
stx720 <michal.obrebski2002@gmail.com>
2023-09-06 02:36:07 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,20 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Weather',
-  description: '',
-}
+  title: "Weather",
+  description: "",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">{children}</body>
+      <body className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
