c3dcb128a12b345259e9b858e51053f1afffc4c3
stx720 <michal.obrebski2002@gmail.com>
2023-09-06 15:15:25 +0200

    Weather icons and style changes
    Now weather icons changes according to current weather(clear, sunny etc.).  Now Enter can be pressed to search in input bar

--- app/page.js
+++ app/page.js
@@ -4,9 +4,10 @@ import Image from "next/image";
 import axios from "axios";
 
 export default function Home() {
-  const [city, setCity] = useState(null);
+  const [city, setCity] = useState("");
   const [cityName, setCityName] = useState(null);
   const [temperature, setTemperature] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
   const handleCityChange = (event) => {
     setCity(event.target.value);
   };
@@ -16,47 +17,82 @@ export default function Home() {
     axios
       .get(api)
       .then((response) => {
-        const { main, name } = response.data;
+        const { main, name, weather } = response.data;
         const { temp } = main;
         setTemperature(temp);
         setCityName(name);
+        setCurrentWeather(weather[0].main);
+        console.log(weather);
       })
       .catch((error) => {
         console.error("Error fetching weather data:", error);
       });
   };
 
+
+  const getWeatherImage = (description) => {
+    switch (description) {
+      case "Snow":
+        return "/snow.png";
+      case "Clear":
+        return "/clear.png";
+      case "Rain":
+        return "/rain.png";
+      case "Clouds":
+        return "/clouds.png";
+    }
+  };
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleFetchWeather(); // Wywołaj funkcję pobierania pogody po naciśnięciu Enter
+    }
+  };
+
   return (
-    <main className="h-screen flex items-center justify-center">
-      <div className="h-72 w-4/12 rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
+    <main className="h-screen flex items-center justify-center flex-col">
+      <h1 className="flex font-extrabold text-white  2xl:text-8xl md:text-8xl sm:text-5xl  ">Weather</h1>
+      <div className="h-72 2xl:w-4/12 md:w-7/12 sm:w-9/12  rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1" >
         <div className=" h-full w-full bg-gray-800 flex items-center justify-center ">
-        <div className="flex-col">
-          {temperature !== null ? (
-              <h1 className="text-white text-5xl">{temperature}°C</h1>
+          <div className="flex-col mr-10">
+          {currentWeather && (
+              <Image
+                src={getWeatherImage(currentWeather)}
+                width={96}
+                height={96}
+                alt="Weather Icon"
+                  className="mx-auto mb-2"
+                />
+                )}
+                <h1 className="text-white text-4xl whitespace-nowrap">{cityName}</h1>
+            {temperature !== null ? (
+              <h1 className="text-white text-2xl mt-2">{temperature.toFixed(1)}°C</h1>
             ) : (
               <p className="text-white"></p>
             )}
-           
-           
+          
           </div>
+          
           <input
             type="text"
             placeholder="Search city..."
             value={city}
+            onKeyPress={handleKeyPress}
             onChange={handleCityChange}
             className="flex w-4/12 h-8 bg-gray-500 text-white pl-1 rounded-md   "
           />
-          <button onClick={handleFetchWeather}>
+          <button onClick={handleFetchWeather} className="mr-2">
             {" "}
             <Image
               src="/search.svg"
               width={32}
               height={32}
               alt="Picture of the author"
-              className="bg-gradient-to-r from-violet-700 via-violet-500 to-pink-500 ml-1 rounded-md   "
+              className="bg-gradient-to-r from-violet-700 via-violet-500 to-pink-500 ml-1 rounded-md hover:animate-pulse   "
             />
           </button>
-          <h1 className="text-white">{cityName}</h1>
+          
+          
+        
         </div>
       </div>
     </main>
