01fc8acd33740f0b1f8294496a49dd3b487e2a3a
stx720 <michal.obrebski2002@gmail.com>
2023-09-12 23:20:59 +0200

    Autosuggest list in input
    Suggestion list that takes from json. It kinda works but also is buggy. Experimental!

--- app/page.js
+++ app/page.js
@@ -8,6 +8,8 @@ import "react-toastify/dist/ReactToastify.css";
 import "animate.css";
 import { DateTime } from "luxon";
 import { IANAZone } from "luxon";
+import citiesData from "./api/cities.json";
+import Autosuggest from "react-autosuggest";
 
 export default function Home() {
   const [city, setCity] = useState("");
@@ -17,6 +19,8 @@ export default function Home() {
   const [isNight, setIsNight] = useState(false);
   const [timezone, setTimezone] = useState("");
   const [country, setCountry] = useState("");
+  const [value, setValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
   const handleCityChange = (event) => {
     setCity(event.target.value);
     toast.dismiss();
@@ -41,7 +45,7 @@ export default function Home() {
         console.error("Error fetching weather data:", error);
         toast.warning("City not found. Please enter a valid city name.", {
           position: "top-right", // toast position
-          theme:"dark", // toast theme
+          theme: "dark", // toast theme
           autoClose: 5000, // toast display time
         });
       });
@@ -96,6 +100,63 @@ export default function Home() {
     setIsNight(hour >= 20 || hour < 6);
   }, [timezone]);
 
+  //autosuggest section
+
+  const citySuggestions = citiesData.map((city) => ({
+    name: `${city.name}, ${city.country}`,
+    id: city.id,
+  }));
+  const renderSuggestion = (suggestion) => <div>{suggestion.name}</div>;
+
+  const onSuggestionsFetchRequested = ({ value }) => {
+    setTimeout(() => {
+      const inputValue = value.trim().toLowerCase();
+      const inputLength = inputValue.length;
+
+      const filteredSuggestions =
+        inputLength === 0
+          ? []
+          : citySuggestions.filter(
+              (suggestion) =>
+                suggestion.name.toLowerCase().slice(0, inputLength) === inputValue
+            );
+            const uniqueSuggestions = Array.from(new Set(filteredSuggestions.map((suggestion) => suggestion.name))).map((name) => {
+              return filteredSuggestions.find((suggestion) => suggestion.name === name);
+            }); //
+
+            setSuggestions(uniqueSuggestions.slice(0, 3));
+    }, 1000);
+  };
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
+
+  const onSuggestionSelected = (event, { suggestion }) => {
+    setValue(suggestion.name);
+    setSuggestions([]);
+    setCity(suggestion.name); // Set the selected city as the current input value
+    handleFetchWeather(); // Trigger weather fetch when a suggestion is selected
+  };
+
+  const renderInputComponent = (inputProps) => (
+    <input
+      {...inputProps}
+      className="animate__animated animate__fadeInDown flex 2xl:text-base text-sm w-full h-8 bg-gray-600 text-white pl-1 rounded-l-lg outline-none transition duration-500 focus:outline-purple focus:border-purple-500 focus:bg-gray-200 focus:text-black focus:duration-500"
+    />
+  );
+
+  const inputProps = {
+    placeholder: "Search city...",
+    value: city,
+
+    onChange: (event, { newValue }) => {
+      setCity(newValue);
+      handleCityChange;
+      toast.dismiss();
+    },
+    onKeyPress: handleKeyPress,
+  };
+
   return (
     <main className="h-screen flex items-center justify-center flex-col  ">
       <h1 className="flex font-extrabold text-white mb-2 2xl:text-8xl text-7xl animate__animated animate__fadeInDown ">
@@ -128,13 +189,47 @@ export default function Home() {
               </h1>
             </div>
           )}
-          <input
-            type="text"
-            placeholder="Search city..."
-            value={city}
-            onKeyPress={handleKeyPress}
-            onChange={handleCityChange}
-            className="animate__animated animate__fadeInDown flex 2xl:text-base text-sm w-4/12 h-8 bg-gray-600 text-white pl-1 rounded-l-lg outline-none transition duration-500 focus:outline-purple focus:border-purple-500 focus:bg-gray-200 focus:text-black focus:duration-500  "
+          <Autosuggest
+            suggestions={suggestions}
+            onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+            onSuggestionsClearRequested={onSuggestionsClearRequested}
+            onSuggestionSelected={onSuggestionSelected}
+            getSuggestionValue={(suggestion) => suggestion.name}
+            renderSuggestion={renderSuggestion}
+            inputProps={inputProps}
+            renderInputComponent={renderInputComponent}
+            // Dodaj styl CSS na kontenerze sugestii
+            shouldRenderSuggestions={() => true} // Pokazuj sugestie nawet jeśli pole jest puste
+            containerProps={{
+              style: {
+                position: "relative",
+              },
+            }}
+            theme={{
+              container: {
+                position: "relative",
+                width: "100%", // Ustaw szerokość na 100% lub inną odpowiednią wartość
+                height: "200px", // Ustaw stałą wysokość na kontenerze sugestii
+              },
+              suggestionsList: {
+                position: "absolute",
+                listStyle: "none",
+                width: "100%",
+                borderStyle: "solid",
+                margin: 0,
+                padding: 0,
+                zIndex: 1,
+                border: "2px solid black", // Dodaj odpowiednie style do listy sugestii
+                backgroundColor: "white", // Dodaj odpowiedni kolor tła
+                overflowY: "auto", // Dodaj pionowy pasek przewijania, jeśli jest potrzebny
+                borderRadius : "10px",
+              },
+              suggestion: {
+                padding: "10px", // Dodaj odpowiednie style do pojedynczej sugestii
+                border: "1px solid black",
+                cursor: "pointer",
+              },
+            }}
           />
           <button onClick={handleFetchWeather} className="mr-2">
             {" "}
